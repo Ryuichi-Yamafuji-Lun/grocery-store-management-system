282db132982f6b9cdad12117e65028b953ab0c65
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-07-01 17:46:32 +0900

    implement basic mysql function of insert delete and print

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -1,7 +1,6 @@
 from sql_connection import get_mysql_connection
 
 def get_all_products(connection):
-
     cursor = connection.cursor()
 
     query = ("SELECT products.product_id, products.name, products.uom_id, products.price_per_unit, uom.uom_name FROM grocerystore.products inner join grocerystore.uom on products.uom_id=uom.uom_id ")
@@ -24,7 +23,29 @@ def get_all_products(connection):
 
     return response
 
+def insert_new_product(connection, product):
+    cursor = connection.cursor()
+
+    query = ("INSERT INTO grocerystore.products (name, uom_id, price_per_unit) values (%s,%s,%s)")
+
+    data = (product['product_name'],product['uom_id'],product['price_per_unit'])
+
+    cursor.execute(query, data)
+
+    connection.commit()
+
+    return cursor.lastrowid
+
+def delete_product(connection,product_id):
+    cursor = connection.cursor()
+
+    query = ("DELETE FROM grocerystore.products WHERE product_id =" + str(product_id))
+    cursor.execute(query)
+    connection.commit()
+
+
+
 if __name__ == "__main__":
     
     connection = get_mysql_connection()
-    print(get_all_products(connection))
+    print(delete_product(connection, 2))
--- backend/sql_connection.py
+++ backend/sql_connection.py
@@ -1,11 +1,11 @@
 import mysql.connector
 
-__cnx == None
+__cnx = None
 
 def get_mysql_connection():
     global __cnx
     if __cnx == None:
-        __cnx = mysql.connector.connect(user='root', password='60ca563b5c0e',
+        __cnx = mysql.connector.connect(user='root', password='root',
                                     host='127.0.0.1',
                                     database='grocerystore')
     return __cnx
