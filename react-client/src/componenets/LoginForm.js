ce1a12b3612193ab8262afb3a636124ee147b86c
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-07-08 19:48:37 +0900

    Create interactive login page

--- react-client/src/componenets/LoginForm.js
+++ react-client/src/componenets/LoginForm.js
@@ -1,18 +1,31 @@
+import { useState } from 'react'
 
 const LoginForm = () => {
+  const [popupStyle, showPopup] = useState("hide")
+
+  const popup = () => {
+    showPopup("login-popup")
+    setTimeout(() => showPopup("hide"), 3000)
+  }
+
   return (
     <div className="login-page">
         <h1>Login</h1>
         <input type="text" placeholder="username"/>
         <input type="password" placeholder="password"/>
 
-        <input type="submit" className="login-btn">Login</input>
+        <div className="login-btn" onClick={popup}>Login</div>
 
-        <p className="text">Or login using</p>\
+        <p className="text">Or login using</p>
 
         <div className="alt-login">
             <div className="facebook"></div>
             <div className="google"></div>
+        </div>
+
+        <div className={popupStyle}>
+          <h3>Login Failed</h3>
+          <p>Username or Password is Incorrect</p>
         </div>
     </div>
   )
