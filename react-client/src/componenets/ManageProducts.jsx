b04c92aebeb5db5c79a37b24b1ec9f2cf7ba2a91
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-07-26 00:51:29 +0900

    build skeleton for neworder and manageproducts

--- react-client/src/componenets/ManageProducts.jsx
+++ react-client/src/componenets/ManageProducts.jsx
@@ -7,7 +7,7 @@ const ManageProducts = () => {
   
   useEffect(() => {
     const backendURL = process.env.REACT_APP_BACKEND_URL;
-    axios.get(`${backendURL}/getAllOrders`).then(
+    axios.get(`${backendURL}/getProducts`).then(
       response => {
         setData(response.data)
       }
@@ -20,30 +20,27 @@ const ManageProducts = () => {
 <div name='main' className="w-full h-screen">
       <div className="mx-auto flex flex-row justify-between items-center p-4">
         <div className="text-4xl">
-          Orders
+          Manage Products
         </div>
         <div>
-          <a href="/manageproducts" className="bg-[#204e93] text-gray-100 py-2 px-3 mx-2 transition hover: scale-115 ">Manage Products</a>
-          <a href="/neworder" className="bg-[#204e93] text-gray-100 py-2 px-3 mx-2 transition hover: scale-115 ">New Orders</a>
+          <a href="/manageproducts" className="bg-[#204e93] text-gray-100 py-2 px-3 mx-2 transition hover: scale-115 ">Add</a>
         </div>        
       </div>
       <div className="mx-auto flex flex-row justify-center items-center p-4">
         <table className="shadow-lg bg-white w-full">
           <thead>
             <tr className="bg-blue-100">
-              <th className="border text-center px-8 py-4">Product #</th>
-              <th className="border text-center px-8 py-4">Product</th>
-              <th className="border text-center px-8 py-4">Customer</th>
-              <th className="border text-center px-8 py-4">Total Cost</th>
+              <th className="border text-center px-8 py-4">Name</th>
+              <th className="border text-center px-8 py-4">Unit</th>
+              <th className="border text-center px-8 py-4">Price per unit</th>
             </tr>
           </thead>
           <tbody>
             {data.map(data => (
-              <tr key = { data.order_id }>
-                <td className="py-2 px-4 text-center">{data.date}</td>
-                <td className="py-2 px-4 text-center">{data.order_id}</td>
-                <td className="py-2 px-4 text-center">{data.customer_name}</td>
-                <td className="py-2 px-4 text-center">{data.total}</td>
+              <tr key = { data.product_id }>
+                <td className="py-2 px-4 text-center">{data.name}</td>
+                <td className="py-2 px-4 text-center">{data.uom_name}</td>
+                <td className="py-2 px-4 text-center">{data.price_per_unit}</td>
               </tr>
             ))}
           </tbody>
--- react-client/src/componenets/NewOrder.jsx
+++ react-client/src/componenets/NewOrder.jsx
@@ -1,14 +1,47 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const NewOrder = () => {
+  const [data, setData] = useState([{}])
+  
+  useEffect(() => {
+    const backendURL = process.env.REACT_APP_BACKEND_URL;
+    axios.get(`${backendURL}/getProducts`).then(
+      response => {
+        setData(response.data)
+      }
+    ).catch(error => {
+      console.error("Error fetching data:", error);
+    });
+  }, [])
+
   return (
-    <div name='main' className="w-full h-screen">
-      <div className=" mx-auto flex flex-row justify-between items-center p-4">
+<div name='main' className="w-full h-screen">
+      <div className="mx-auto flex flex-row justify-between items-center p-4">
         <div className="text-4xl">
           New Order
-        </div>
-        <div>
-          <a href="/test" className="bg-[#204e93] text-gray-100 py-2 px-3 mx-2 transition hover: scale-115 ">Add</a>
-        </div>        
+        </div>      
+      </div>
+      <div className="mx-auto flex flex-row justify-center items-center p-4">
+        <table className="shadow-lg bg-white w-full">
+          <thead>
+            <tr className="bg-blue-100">
+              <th className="border text-center px-8 py-4">Product</th>
+              <th className="border text-center px-8 py-4">Price per unit</th>
+              <th className="border text-center px-8 py-4">Quantity</th>
+              <th className="border text-center px-8 py-4">Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map(data => (
+              <tr key = { data.product_id }>
+                <td className="py-2 px-4 text-center">{data.name}</td>
+                <td className="py-2 px-4 text-center">{data.uom_name}</td>
+                <td className="py-2 px-4 text-center">{data.price_per_unit}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   )
