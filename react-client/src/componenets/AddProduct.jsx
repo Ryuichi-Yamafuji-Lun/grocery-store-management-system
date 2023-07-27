778fa8040290e5dc220e374dbffa330a74d463e0
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-07-27 17:35:02 +0900

    make a return button to manageproducts

--- react-client/src/componenets/AddProduct.jsx
+++ react-client/src/componenets/AddProduct.jsx
@@ -7,7 +7,7 @@ const AddProduct = () => {
           Add Products
         </div>
         <div>
-          <a href="/test" className="bg-[#204e93] text-gray-100 py-2 px-3 mx-2 transition hover: scale-115 ">Add</a>
+          <a href="/manageproducts" className="bg-[#204e93] text-gray-100 py-2 px-3 mx-2 transition hover: scale-115 ">Return</a>
         </div>        
       </div>
     </div>
