6cadc6a947654465374097f689aec1e25c57a3fd
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-08-03 22:30:27 +0900

    Update MainPage to take in case of no orders and fixed key unique error

--- react-client/src/componenets/MainPage.jsx
+++ react-client/src/componenets/MainPage.jsx
@@ -2,14 +2,14 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const MainPage = () => {
-  const [order, setOrder] = useState([{}]);
+  const [orders, setOrders] = useState([]);
   const [orderTotals, setOrderTotals] = useState({});
   
   useEffect(() => {
     const backendURL = process.env.REACT_APP_BACKEND_URL;
     axios.get(`${backendURL}/getAllOrders`).then(
       response => {
-        setOrder(response.data);
+        setOrders(response.data);
         //Getting order total
         const totals = {};
         response.data.forEach((order) => {
@@ -34,28 +34,32 @@ const MainPage = () => {
           <a href="/neworder" className="bg-[#204e93] text-gray-100 py-2 px-3 mx-2 transition hover: scale-115 ">New Orders</a>
         </div>        
       </div>
-      <div className="mx-auto flex flex-row justify-center items-center p-4">
-        <table className="shadow-lg bg-white w-full">
-          <thead>
-            <tr className="bg-blue-100">
-              <th className="border text-center px-8 py-4">Date</th>
-              <th className="border text-center px-8 py-4">Order #</th>
-              <th className="border text-center px-8 py-4">Customer</th>
-              <th className="border text-center px-8 py-4">Total Cost</th>
-            </tr>
-          </thead>
-          <tbody>
-            {order.map( order => (
-              <tr key = { order.order_id }>
-                <td className="py-2 px-4 text-center">{order.date}</td>
-                <td className="py-2 px-4 text-center">{order.order_id}</td>
-                <td className="py-2 px-4 text-center">{order.customer_name}</td>
-                <td className="py-2 px-4 text-center">{orderTotals[order.order_id]}</td>
+      {orders.length > 0 ? (
+        <div className="mx-auto flex flex-row justify-center items-center p-4">
+          <table className="shadow-lg bg-white w-full">
+            <thead>
+              <tr className="bg-blue-100">
+                <th className="border text-center px-8 py-4">Date</th>
+                <th className="border text-center px-8 py-4">Order #</th>
+                <th className="border text-center px-8 py-4">Customer</th>
+                <th className="border text-center px-8 py-4">Total Cost</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {orders.map((order) => (
+                <tr key={order.order_id}>
+                  <td className="py-2 px-4 text-center">{order.date}</td>
+                  <td className="py-2 px-4 text-center">{order.order_id}</td>
+                  <td className="py-2 px-4 text-center">{order.customer_name}</td>
+                  <td className="py-2 px-4 text-center">{orderTotals[order.order_id]}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <div className="mx-auto p-4 text-center">No orders available.</div>
+      )}
     </div>
   )
 }
