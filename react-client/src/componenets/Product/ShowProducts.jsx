6f8fb183ccebb8209d8c0937000020208cbb962d
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-08-09 16:42:30 +0900

    fixed the unique key issue and no products issue

--- react-client/src/componenets/Product/ShowProducts.jsx
+++ react-client/src/componenets/Product/ShowProducts.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const ShowProducts = () => {
   
-  const [products, setProducts] = useState([{}]);
+  const [products, setProducts] = useState([]);
   
   useEffect(() => {
     const backendURL = process.env.REACT_APP_BACKEND_URL;
@@ -26,26 +26,30 @@ const ShowProducts = () => {
           <a href="/manageproducts" className="bg-[#204e93] text-gray-100 py-2 px-3 mx-2 transition hover: scale-115 ">Manage Products</a>
         </div>        
       </div>
-      <div className="mx-auto flex flex-row justify-center items-center p-4">
-        <table className="shadow-lg bg-white w-full">
-          <thead>
-            <tr className="bg-blue-100">
-              <th className="border text-center px-8 py-4">Name</th>
-              <th className="border text-center px-8 py-4">Unit</th>
-              <th className="border text-center px-8 py-4">Price per unit</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map( products => (
-              <tr key = { products.product_id }>
-                <td className="py-2 px-4 text-center">{products.name}</td>
-                <td className="py-2 px-4 text-center">{products.uom_name}</td>
-                <td className="py-2 px-4 text-center">{products.price_per_unit}</td>
+      {products.length > 0 ? (
+        <div className="mx-auto flex flex-row justify-center items-center p-4">
+          <table className="shadow-lg bg-white w-full">
+            <thead>
+              <tr className="bg-blue-100">
+                <th className="border text-center px-8 py-4">Name</th>
+                <th className="border text-center px-8 py-4">Unit</th>
+                <th className="border text-center px-8 py-4">Price per unit</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {products.map( products => (
+                <tr key = { products.product_id }>
+                  <td className="py-2 px-4 text-center">{products.name}</td>
+                  <td className="py-2 px-4 text-center">{products.uom_name}</td>
+                  <td className="py-2 px-4 text-center">{products.price_per_unit}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ): (
+        <div className="mx-auto p-4 text-center">No products available.</div>
+      )}
     </div>
   )
 }
