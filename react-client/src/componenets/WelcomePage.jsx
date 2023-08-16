6364b841855cf62ba4e9d9a7e1e63d5895dcbb92
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-08-16 18:11:39 +0900

    Apply pokemon font and theme to welcomepage

--- react-client/src/componenets/WelcomePage.jsx
+++ react-client/src/componenets/WelcomePage.jsx
@@ -1,14 +1,15 @@
-import pikachu from '../assets/pokemon-sprites/Pokemon/pikachu.gif'
+import pikachu from '../assets/pokemon-sprites/Pokemon/pikachu.gif';
 
 const WelcomePage = () => {
-
   return (
-    <div className="w-full h-screen"> 
-      <div className="max-w-[1000px] mx-auto px-8 flex items-center flex-col justify-center h-full">
-        <h1 className="text-4xl sm:text-7xl font-bold"> Welcome </h1>
-        <p className="text-2xl py-4 max-w-[700px]">PokéStore Management System</p>
-        <p className="max-w-[700px]"> Made with React, mySQL, and Flask</p>
-        <img src={pikachu} alt="Pikachu"/>
+    <div className="welcome-page flex flex-col justify-center items-center h-screen bg-blue-800">
+      <div className="max-w-[1000px] mx-auto px-8 text-center">
+        <div className="flex items-center justify-center md:justify-start mb-4">
+          <h1 className="text-4xl sm:text-7xl font-bold text-white">Welcome</h1>
+          <img src={pikachu} alt="Pikachu" className="w-1/4 md:w-1/6 ml-4" />
+        </div>
+        <p className="text-2xl py-2 max-w-[700px] text-white">PokéStore Management System</p>
+        <p className="text-white">Made with React, mySQL, and Flask</p>
       </div>
     </div>
   )
