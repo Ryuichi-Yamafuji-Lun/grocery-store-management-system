9adbcafa3077d3d971442ca50c7475d4ad033a06
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-08-21 21:48:25 +0900

    Update About.jsx

--- react-client/src/components/About.jsx
+++ react-client/src/components/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
           Welcome to the PokéStore Management System Web Application! This comprehensive CRUD (CREATE, READ, UPDATE, DELETE) 
           project has been designed to facilitate efficient order and product management for the PokéMart store within the game. 
           The development of this project utilizes a powerful combination of mySQL, Flask, and React technologies. The application 
-          boasts an engaging user interface, ensuring an interactive and enjoyable experience. Moreover, be sure to keep an eye out for hidden interactive easter eggs 
+          boasts an engaging user interface, ensuring an interactive, responsive, and enjoyable experience. Moreover, be sure to keep an eye out for hidden interactive easter eggs 
           strategically placed throughout for added enjoyment! 
           </p>
           <p>Interface:</p>
