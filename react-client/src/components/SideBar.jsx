f89087118adf8d8c9561c0a79fc70b749470d652
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-10-12 15:51:20 +0900

    Add sidebar

--- react-client/src/components/SideBar.jsx
+++ react-client/src/components/SideBar.jsx
@@ -1,6 +1,10 @@
+import { IoMdHome,IoIosListBox } from "react-icons/io";
+import { BsFillQuestionSquareFill, BsArrowLeftShort } from "react-icons/bs";
 
 export const SideBar = () => {
   return (
-    <div>SideBar</div>
+    <div className=""> 
+      
+    </div>
   )
 }
