9290abe4bc620c51b801a008de4c29f4017fa699
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-10-18 16:41:26 +0900

    Update Sidebar style

--- react-client/src/components/SideBar.jsx
+++ react-client/src/components/SideBar.jsx
@@ -6,7 +6,7 @@ import { BsFillQuestionSquareFill, BsArrowLeftShort } from "react-icons/bs";
 const SideBar = () => {
   return (
     <div className="fixed top-0 left-0 z-40 w-auto h-screen transition-transform -translate-x-full xl:translate-x-0 bg-dark-green">
-      <div className="h-full px-4 py-4 overflow-y-auto shadow-lg">
+      <div className="h-full flex flex-col justify-between px-4 py-4 overflow-y-auto shadow-lg">
         <ul className="space-y-2 font-medium">
           <li>
             <Link to="/mainpage" className="flex items-center p-2 text-white rounded-lg hover:backdrop-blur-3xl hover:bg-slate-800/50 hover:shadow-[inset_0px_1px_0px_0px_rgba(148,163,184,0.1)] group">
@@ -33,7 +33,7 @@ const SideBar = () => {
             </Link>
           </li>
         </ul>
-        <div className="mt-auto">
+        <div>
           <Link to="/" className="flex items-center p-2 text-white rounded-lg hover:backdrop-blur-3xl hover:bg-slate-800/50 hover:shadow-[inset_0px_1px_0px_0px_rgba(148,163,184,0.1)] group">
             <BsArrowLeftShort className="w-5 h-5 text-white transition duration-75" />
             <span className="ml-3">Exit</span>
@@ -44,4 +44,4 @@ const SideBar = () => {
   );
 }
 
-export default SideBar;
+export default SideBar
